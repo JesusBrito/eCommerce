96666a38d1a0d8c5b1e74bfa5f6cffe3270a52a4
JesusBrito <jesus291196@gmail.com>
2018-03-15 20:50:20 -0600

    Add the CRUD for the products

--- controllers/product.js
+++ controllers/product.js
@@ -1,9 +1,46 @@
+var dao= require('../dao')
 
+function getProducts(req,res) {
 
-function saveProduct (req,res) {
-	res.status(200).send({message:'OK'});
+	sql="SELECT * FROM prueba";
+	dao.open(sql,[],false,res);
+}
+
+function getProduct(req,res) {
+	var condicion = req.params.id;
+	sql="SELECT * FROM prueba WHERE telefono = :condicion";
+	dao.open(sql,[condicion],false,res);
+}
+
+function saveProduct(req,res){
+	var params= req.body;
+	sql= "INSERT INTO prueba(nombre,telefono) VALUES(:nombre,:telefono)";
+	var nombre=params.nombre;
+	var telefono=params.telefono;
+	dao.open(sql,[nombre,telefono],true,res);
+}
+
+function updateProduct(req,res){
+	var params= req.body;
+	sql= "UPDATE prueba SET nombre = :nombre, telefono = :telefono"+
+					" WHERE telefono = :condicion";
+	var nombre=params.nombre;
+	var telefono=params.telefono;
+	var condicion=params.condicion;
+	dao.open(sql,[nombre,telefono,condicion],true,res);
+}
+
+function deleteProduct(req,res){
+	var params= req.body;
+	sql= "DELETE FROM prueba WHERE telefono = :condicion";
+	var condicion=params.condicion;
+	dao.open(sql,[condicion],true,res);
 }
 
 module.exports={
-	saveProduct
+	getProduct,
+	saveProduct,
+	updateProduct,
+	deleteProduct,
+	getProducts
 }
--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,10 @@ var express = require('express');
 var api=express.Router();
 var ProductController = require('../controllers/product');
 
+api.get('/producto', ProductController.getProducts);
+api.get('/producto/:id', ProductController.getProduct);
 api.post('/producto', ProductController.saveProduct);
+api.put('/producto', ProductController.updateProduct);
+api.delete('/producto', ProductController.deleteProduct);
 
 module.exports= api;
