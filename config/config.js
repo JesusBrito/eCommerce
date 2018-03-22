2f8d9f66852d9b56d9839244aed135d2bf4b58e4
JesusBrito <jesus291196@gmail.com>
2018-03-21 19:10:25 -0600

    config remove

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@ module.exports={
 	port: process.env.PORT||8080,
 	db:{
 		database: process.env.DB_NAME ||'ORCL',
-		user: 'adminjj',
-		password:'juaeuioio',
+		user: '',
+		password:'',
 		options:{
 			host: process.env.HOST ||'instanceoracle.cf21jk1wibrd.us-east-2.rds.amazonaws.com',
 			dialect: process.env.DIALECT ||'oracle',
