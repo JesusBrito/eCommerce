9c5acae88ec354b6e527bfe8cdfbdf329754c274
JesusBrito <jesus291196@gmail.com>
2018-06-01 07:37:32 -0500

    Deploy prueba carga v2

--- config/config.js
+++ config/config.js
@@ -2,10 +2,14 @@ module.exports={
 	port: process.env.PORT||3000,
 	db:{
 		database: process.env.DB_NAME ||'dbApiRest',
-		user: 'eComLoc',
-		password:'juaeu¡o¡o',
+		//database: process.env.DB_NAME ||'instanciamysql.cfusuv1kv2of.us-east-2.rds.amazonaws.com',
+		user: 'JesusBB',
+		password:'JesusBB29',
+		//user: 'eComLoc',
+		//password:'juaeu¡o¡o',
 		options:{
-			host: process.env.HOST ||'localhost',
+			//host: process.env.HOST ||'localhost',
+			host: process.env.HOST ||'instanciamysql.cfusuv1kv2of.us-east-2.rds.amazonaws.com',
 			dialect: process.env.DIALECT ||'mysql',
 			pool:{
 				maxConnections:100,
